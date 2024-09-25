230d2281d8523646f33fcfc30f0d4c7c65d50655
shion <shimizu.shion819@gmail.com>
2024-09-25 10:56:13 +0900

    Updates easifemConfig
    - several changes

--- scripts/edoc.fish
+++
@@ -1,7 +0,0 @@
-function edoc -d "open a easifem doc file in nvim"
-    set -l myloc (pwd)
-    cd $docs
-    cd $docs/(fd --type d | fzf)
-    nvim (fd -I --type f -e md -e F90 -e f90  | fzf )
-    cd $myloc
-end
